140e5d97f5cad58862d2c3202bd3f1cc4b34a55c
Fussita <hualong.chiang@gmail.com>
2025-10-11 22:19:16 -0400

    init: again

--- src/app/app.routes.server.ts
+++
@@ -1,8 +0,0 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: ServerRoute[] = [
-  { path: '', renderMode: RenderMode.Prerender }, // SSG
-  { path: 'about', renderMode: RenderMode.Prerender }, // SSG
-  { path: 'profile', renderMode: RenderMode.Server }, // SSR
-  { path: '**', renderMode: RenderMode.Server }, // FALLBACK SSR
-];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './presentation/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent 
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
