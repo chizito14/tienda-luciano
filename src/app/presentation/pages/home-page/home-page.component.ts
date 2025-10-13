36112b3b45b7de918ec8ae2d86ef9dc78fb779f2
chizito14 <jesusding14@gmail.com>
2025-10-12 22:25:38 -0400

    actualizacion del page

--- src/app/presentation/pages/home-page/home-page.component.ts
+++ src/app/presentation/pages/home-page/home-page.component.ts
@@ -1,13 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { Route, Router, RouterOutlet } from '@angular/router';
+import { Route, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { BestSellerComponent } from '../../widget/best-seller/best-seller.component';
 
 @Component({
     selector: 'home-page',
     standalone: true,
     imports: [
-        RouterOutlet
+        CommonModule,
+        RouterModule,
+        RouterOutlet,
+        BestSellerComponent
     ],
   templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
 
---
+++ src/app/presentation/widget/best-seller/best-seller.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-best-seller',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './best-seller.component.html',
+  styleUrls: ['./best-seller.component.scss'],
+})
+export class BestSellerComponent {}
